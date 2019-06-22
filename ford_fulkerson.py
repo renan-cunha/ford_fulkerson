a3e18f5947e057ad4137d6adc331a8e9c36b9c3f
jvcanavarro <jvcanavarro@gmail.com>
2019-06-22 15:41:29 -0300

    Max Flow

--- ford_fulkerson.py
+++ ford_fulkerson.py
@@ -11,8 +11,10 @@ def min_flow(graph: nx.DiGraph,
     for i in range(num_vertices-1):
         source = path_vertices[i]
         end = path_vertices[i+1]
-        result = min(result, graph.get_edge_data(source, end)["capacity"])
+        result = min(result, graph.get_edge_data(source, end)['capacity'])
         edges.append((source, end))
+
+    print('result: ', result)
     return result, edges
 
 
@@ -22,30 +24,36 @@ def ford_fulkerson(graph: nx.DiGraph, source: int,
     num_edges = len(edges)
     flow = dict(edges)
     flow = {x: 0 for x in flow}
+##    print('flow: ', flow)
     for x, y in edges:
         pass
 
     residual_graph = graph.copy()
-
+    max_flow = 0
     while nx.has_path(residual_graph, source, sink):
+        
         # choose a path
         path = next(nx.all_simple_paths(residual_graph, source, sink))
-
+        print('path: ',path)
+        
         # take the minimal additional flow of this path and the edges
         current_flow, current_edges = min_flow(residual_graph, path)
-
+        max_flow += current_flow
+        
         # update flow values, remove edge and add with new capacity
         for edge in current_edges:
 
             # update flow values
             flow[edge] += current_flow
-            vertice_from, vertice_to = edge
+
+            vertice_from, vertice_to = edge #(u,v)
 
             # update capacity value
             capacity = residual_graph.get_edge_data(vertice_from,
                                                     vertice_to)['capacity']
+            
             capacity -= current_flow
-
+            
             # remove edge
             residual_graph.remove_edge(vertice_from, vertice_to)
 
@@ -53,12 +61,26 @@ def ford_fulkerson(graph: nx.DiGraph, source: int,
             if capacity > 0:
                 residual_graph.add_edge(vertice_from, vertice_to,
                                         capacity=capacity)
+        print('max_flow: ', max_flow)
+            
 
-    return flow
+    return max_flow
 
 
 graph = nx.DiGraph()
-graph.add_edge(0, 7, capacity=10.9)
-graph.add_edge(7, 6, capacity=5.6)
-graph.add_edge(0, 6, capacity=2)
-print(ford_fulkerson(graph, 0, 6))
+graph.add_edge(0, 1, capacity=13)
+graph.add_edge(0, 3, capacity=10)
+graph.add_edge(0, 5, capacity=10)
+graph.add_edge(1, 2, capacity=24)
+graph.add_edge(2, 4, capacity=1)
+graph.add_edge(2, 7, capacity=9)
+graph.add_edge(3, 1, capacity=5)
+graph.add_edge(3, 5, capacity=15)
+graph.add_edge(3, 6, capacity=7)
+graph.add_edge(4, 6, capacity=6)
+graph.add_edge(4, 7, capacity=13)
+graph.add_edge(5, 6, capacity=15)
+graph.add_edge(6, 7, capacity=16)
+
+
+print(ford_fulkerson(graph, 0, 7))
